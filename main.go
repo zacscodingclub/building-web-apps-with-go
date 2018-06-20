25878900fe343b35c962c850c5e287cc53ba66b0
Zac Baston <zbaston@tableau.com>
2018-06-20 15:15:58 -0700

    add Procfile

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/russross/blackfriday"
 )
 
 func main() {
-	fmt.Println("Now serving to localhost:8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Now serving to localhost:", port)
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("templates")))
-	http.ListenAndServe("localhost:8080", nil)
+	http.ListenAndServe("localhost:"+port, nil)
 }
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
